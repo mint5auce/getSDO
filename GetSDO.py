23c0e15681e0edd0933989408fe1914b34167d6f
mint5auce <me@jon-hadley.com>
2016-02-18 14:20:10 +0000

    Download one from each feed

--- GetSDO.py
+++ GetSDO.py
@@ -2,68 +2,74 @@
 #
 # A Python script to download the latest image from NASA's Solar Dynamics
 # Observatory RSS feed.
-# I use it from a launchd job to set the wallpaper on my Mac ;)
 #
-# Requires (pip install -r requirements.txt)
+# I use it to set the screensaver image source on my Mac
 #
-# Growl removed. Caused issues and I don't require notification
-#
-# Universal Feed Parser (https://github.com/kurtmckee/feedparser
+# Requires: Universal Feed Parser (https://github.com/kurtmckee/feedparser
 # / https://pythonhosted.org/feedparser/)
-#       for RSS fetch & parse
-# BeautifulSoup (http://www.crummy.com/software/BeautifulSoup/)
-#       for parsing HTML in the RSS feed
 #
 # Based on BigPicture.py by: Henry Cooke (me@prehensile.co.uk)
 # https://gist.github.com/prehensile/675906
 
 import feedparser
 import urllib2
-from BeautifulSoup import BeautifulSoup
 
 import os
 import time
 
+# Testing
 import pdb
 
 
-# config
-fout = '/Users/jonh/Pictures/sdo-feed/sdo.jpeg'
-bp_rss = 'http://feeds.feedburner.com/nasa/aia_193?format=xml'
+# Config
+fdir = '/Users/jonh/Pictures/sdo-feed/'
+sdo_rss_root = 'http://feeds.feedburner.com/nasa/'
+sdo_rss_views = ['aia_094', 'aia_131', 'aia_171', 'aia_193', 'aia_211', 'aia_304', 'aia_335', 'aia_1600', 'aia_1700', 'COMP094335193', 'COMP211193171', 'COMP304211171', 'COMPHMI171', 'hmib', 'hmibc', 'hmii', 'hmiic'] # hmid & f was offline at time of writing
 
-# check age of existing file
-# @TODO: I want all form last 24 hours. Not 1 for each 24 hours. Or do I...
-# @TODO: Or .... match date to today / last 24 hours from XML
-if( os.access( fout, os.F_OK ) ):
-        stats = os.stat( fout )
-        mtime = stats.st_mtime
-        now = time.time()
-        if( now - mtime < (60 * 60 * 24) ):
-                exit( "Existing file is newer than a day old" );
+# Parse RSS
 
-# Parse rss
-feed = feedparser.parse( bp_rss )
-items = feed[ "items" ]
-url = items[0]['media_content'][0]['url']
+# Get first image from each seperate type
 
-# Gonna want a for loop eventually, to get all images
-# pdb.set_trace()
+for view in sdo_rss_views:
+    sdo_rss = sdo_rss_root + view + '?format=xml'
+    feed = feedparser.parse( sdo_rss )
+    items = feed[ "items" ]
+    url = items[0]['media_content'][0]['url'] # Just get first item
 
+    # Download image
+    try:
+            data = urllib2.urlopen( url ).read()
+            fname = view + '_' + url.split('/')[-1] # Feed images use same name, hence prefix
+            fout = fdir+fname
+            # @TODO: Don't download again if it exisits
+            output = open( fout, 'wb' )
+            output.write( data )
+            output.close()
+            print 'File downloaded: ', fname
+    except:
+            print 'URL error: ', url
+            pass
 
+# @TODO: Remove anything older than one week
 
-# It's all RSS, so pretty sure I don't need Soup, only FeedParser.
-# leading_item = items[ 0 ]
-# soup = BeautifulSoup( leading_item[ "description" ] )
-# Class no longer exists
-# img = soup.find( "img", { "class" : "bpImage" } )
-# img = soup.find( "img" )
-# url = img[ "src" ]
+# DEPRECATED - I want one a day (not enough changes hourly)
+# Get all images from feed - RSS shows last 5 hours or so in 15 min intervals.
 
-# download image
-try:
-        data = urllib2.urlopen( url ).read()
-        output = open( fout, 'wb' )
-        output.write( data )
-        output.close()
-except:
-        pass
+# for item in items:
+#     # pdb.set_trace() # Test
+#     url = item['media_content'][0]['url']
+#     # pdb.set_trace() # Test
+#
+#     # download image
+#     try:
+#             data = urllib2.urlopen( url ).read()
+#             fname = url.split('/')[-1]
+#             fout = fdir+fname
+#             # @TODO: Don't download again if it exisits
+#             output = open( fout, 'wb' )
+#             output.write( data )
+#             output.close()
+#             print 'File downloaded: ', url
+#     except:
+#             print 'File error: ', url
+#             pass
